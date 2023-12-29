e4e13f6ad6b3780b1e2431bdb720afc1b12f86be
nirtendra <nirtendrayadav12@gmail.com>
2023-12-29 22:00:12 +1100

    complete arrow function lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(a,b){
+   return  a/b
+
+
+}
+
+const square = x =>  x**2    //arrow function with single parameter so that it doesnt need paranthesis
+
+
+const add =(a,b) =>{       //arrow function with 2 paramenter
+    return a+b
+}
